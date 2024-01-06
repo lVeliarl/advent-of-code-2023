c8fff6418825084e94ede2daf248a549e9abf714
lVeliarl <dimalitx@gmail.com>
2024-01-06 01:00:44 +0100

    feat: day 1 part 2 solution

--- 01/p1.ts
+++ 01/p1.ts
@@ -1,14 +1,12 @@
 import { readFile } from "fs";
 
-function getDigitSum(text: string) {
-  return text.split('\n').map(s => s.replace(/\D+/g, '')).reduce((acc, curr) => {
-    return acc + Number(curr[0] + curr[curr.length - 1]);
-  }, 0);
+function getCalibrationValue(text: string) {
+  return text.split('\n').map(s => s.replace(/\D+/g, '')).reduce((acc, curr) => acc + Number(curr[0] + curr[curr.length - 1]), 0);
 }
 
 readFile('./01/input.txt', 'utf-8', async(err, data) => {
   if (err) 
     console.log(err)
 
-  console.log(getDigitSum(data)); // 55488
+  console.log(getCalibrationValue(data)); // 55488
 });
---
+++ 01/p2.ts
@@ -0,0 +1,28 @@
+import { readFile } from "fs";
+
+const digitMap: Record<string, string> = {
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
+};
+
+const rr = /(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g;
+
+function getCalibrationValue(text: string) {
+  const matchArr = text.split('\n').map(s => [...s.matchAll(rr)]?.map(d => digitMap[d[1]] || d[1]));
+
+  return matchArr ? matchArr.reduce((acc, curr) => acc + Number((curr[0] + curr[curr.length - 1])), 0) : null;
+};
+
+readFile('./01/input.txt', 'utf-8', async(err, data) => {
+  if (err) 
+    console.log(err)
+
+  console.log(getCalibrationValue(data)); // 55614
+});
